93406b8ceeb0594734ad7fda88ec256fd2dbbcf3
Kenta Ogawa <v19e3009@oita-u.ac.jp>
2020-05-18 06:57:22 +0000

    Lesson 03課題

---
+++ hello.rb
@@ -0,0 +1,28 @@
+#coment
+
+=begin
+コメント
+コメント
+コメント
+コメント
+=end
+
+print "hello world"
+puts "hello world" # +new_line
+p "hello world" # for_debug
+
+#変数
+
+msg = "hello world"
+puts msg
+
+msg = "hello world again"
+puts msg
+
+#定数
+
+VERSION = 1.1
+puts VERSION
+
+VERSION = 1.2
+puts VERSION
